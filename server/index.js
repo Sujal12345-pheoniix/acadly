311083bb598d96eeacad6baaf097f90c5952e3c7
Sujal12345-pheoniix <sujalreal983@gmail.com>
2026-05-01 23:05:44 +0530

    final

--- server/index.js
+++ server/index.js
@@ -63,8 +63,8 @@ app.get("/", (req, res) => {
 		message:'Your server is up and running....'
 	});
 });
+// const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-	console.log(`App is running at ${PORT}`)
-})
-
+  console.log(`Server running on port ${PORT}`);
+});
